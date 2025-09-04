2584c7808d40254156ebc7789afe0f596c19f983
bgkillas <walhelm@proton.me>
2025-09-03 21:54:10 -0400

    physics, wayland, raycast

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use bevy::asset::RenderAssetUsages;
 use bevy::input::mouse::AccumulatedMouseMotion;
 use bevy::prelude::*;
 use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
+use bevy::window::PrimaryWindow;
+use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
+use bevy_rapier3d::prelude::*;
 use futures::StreamExt;
 use futures::stream::FuturesUnordered;
 use image::{GenericImageView, ImageReader};
@@ -11,8 +14,9 @@ use std::fs;
 use std::io::Cursor;
 use std::sync::LazyLock;
 use tokio::task::JoinHandle;
-
 static USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+static CARD_WIDTH: f32 = 488.0;
+static CARD_HEIGHT: f32 = 680.0;
 fn main() {
     let runtime = Runtime(tokio::runtime::Runtime::new().unwrap());
     let client = Client(
@@ -22,8 +26,20 @@ fn main() {
             .unwrap(),
     );
     let clipboard = Clipboard(LazyLock::new(|| arboard::Clipboard::new().unwrap()));
+    let app_window = Some(Window {
+        title: "rmtg".into(),
+        ..default()
+    });
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: app_window,
+                ..default()
+            }),
+            RapierPhysicsPlugin::<NoUserData>::default(),
+            RapierDebugRenderPlugin::default(),
+            FramepacePlugin,
+        ))
         .insert_resource(clipboard)
         .insert_resource(runtime)
         .insert_resource(client)
@@ -35,16 +51,70 @@ fn main() {
                 register_deck,
                 cam_translation,
                 cam_rotation,
+                listen_for_mouse,
             ),
         )
         .run();
 }
-fn cam_translation(
-    input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut Transform, &Camera3d)>,
+#[allow(clippy::too_many_arguments)]
+fn listen_for_mouse(
+    input: Res<ButtonInput<MouseButton>>,
+    camera: Single<(&Camera, &GlobalTransform)>,
+    window: Single<&Window, With<PrimaryWindow>>,
+    rapier_context: ReadRapierContext,
+    mut cards: Query<(&mut Pile, &Transform, &Children)>,
+    mut card_mats: Query<&mut MeshMaterial3d<StandardMaterial>>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    card_back: Res<CardBack>,
+    card_stock: Res<CardStock>,
 ) {
-    let (cam, _) = query.iter_mut().next().unwrap();
-    let cam = cam.into_inner();
+    if input.just_pressed(MouseButton::Right) {
+        let Some(cursor_position) = window.cursor_position() else {
+            return;
+        };
+        let (camera, camera_transform) = camera.into_inner();
+        let Ok(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+            return;
+        };
+        let Ok(context) = rapier_context.single() else {
+            return;
+        };
+        context.with_query_pipeline(QueryFilter::only_dynamic(), |query_pipeline| {
+            let hit = query_pipeline.cast_ray(ray.origin, ray.direction.into(), f32::MAX, true);
+            if let Some((entity, _toi)) = hit
+                && let Ok((pile, transform, children)) = cards.get_mut(entity)
+            {
+                let pile = pile.into_inner();
+                let new = pile.0.pop().unwrap();
+                if pile.0.is_empty() {
+                    commands.entity(entity).despawn()
+                } else {
+                    let top = pile.0.last().unwrap();
+                    let mat = make_material(&mut materials, top.image.clone_weak());
+                    for child in children {
+                        let mut old_mat = card_mats.get_mut(*child).unwrap();
+                        if old_mat.0.id() != card_back.0.id() {
+                            old_mat.0 = mat;
+                            break;
+                        }
+                    }
+                }
+                new_pile(
+                    vec![new],
+                    card_stock.0.clone_weak(),
+                    &mut materials,
+                    &mut commands,
+                    card_back.0.clone_weak(),
+                    transform.translation.x,
+                    transform.translation.z - CARD_HEIGHT,
+                );
+            }
+        });
+    }
+}
+fn cam_translation(input: Res<ButtonInput<KeyCode>>, cam: Single<(&mut Transform, &Camera3d)>) {
+    let cam = cam.into_inner().0.into_inner();
     if input.pressed(KeyCode::KeyW) {
         cam.translation += cam.forward().as_vec3() * 32.0;
     }
@@ -58,16 +128,15 @@ fn cam_translation(
         cam.translation += cam.back().as_vec3() * 32.0;
     }
     if input.pressed(KeyCode::Space) {
-        *cam = Transform::from_xyz(0.0, 2048.0, -2048.0).looking_at(Vec3::ZERO, Vec3::Z);
+        *cam = Transform::from_xyz(0.0, 2048.0, -2048.0).looking_at(Vec3::ZERO, Vec3::Y);
     }
 }
 fn cam_rotation(
     mouse_button: Res<ButtonInput<MouseButton>>,
     mouse_motion: Res<AccumulatedMouseMotion>,
-    mut query: Query<(&mut Transform, &Camera3d)>,
+    cam: Single<(&mut Transform, &Camera3d)>,
 ) {
-    let (cam, _) = query.iter_mut().next().unwrap();
-    let cam = cam.into_inner();
+    let cam = cam.into_inner().0.into_inner();
     if mouse_button.pressed(MouseButton::Left) && mouse_motion.delta != Vec2::ZERO {
         let delta_yaw = -mouse_motion.delta.x * 0.001;
         let delta_pitch = -mouse_motion.delta.y * 0.001;
@@ -84,10 +153,10 @@ fn setup(
     runtime: Res<Runtime>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut framepace: ResMut<FramepaceSettings>,
 ) {
-    let width = 488.0;
-    let height = 680.0;
-    let card_stock = meshes.add(Rectangle::new(width, height));
+    framepace.limiter = Limiter::from_framerate(60.0);
+    let card_stock = meshes.add(Rectangle::new(CARD_WIDTH, CARD_HEIGHT));
     let card_back = asset_server.load("back2.png");
     let material_handle = materials.add(StandardMaterial {
         base_color_texture: Some(card_back),
@@ -95,22 +164,31 @@ fn setup(
         unlit: true,
         ..default()
     });
+    let card = get_from_img(
+        Cursor::new(&fs::read("/home/.r/rmtg/assets/png.png").unwrap()),
+        asset_server.clone(),
+    )
+    .unwrap();
     new_pile(
         vec![
-            get_from_img(
-                Cursor::new(&fs::read("/home/.r/rmtg/assets/png.png").unwrap()),
-                asset_server.clone(),
-            )
-            .unwrap(),
+            Card {
+                image: card.image.clone_weak(),
+            },
+            card,
         ],
         card_stock.clone_weak(),
         &mut materials,
         &mut commands,
         material_handle.clone_weak(),
         0.0,
+        0.0,
     );
     commands.insert_resource(CardBack(material_handle));
     commands.insert_resource(CardStock(card_stock));
+    commands.spawn((
+        Transform::from_xyz(0.0, -1.0, 0.0),
+        Collider::cuboid(4096.0, 1.0, 4096.0),
+    ));
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 2048.0, -2048.0).looking_at(Vec3::ZERO, Vec3::Z),
@@ -289,52 +367,66 @@ fn test_get_deck() {
 }
 fn register_deck(
     mut commands: Commands,
-    mut query: Query<(Entity, &mut GetDeck)>,
+    query: Single<(Entity, &mut GetDeck)>,
     runtime: Res<Runtime>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     card_back: Res<CardBack>,
     card_stock: Res<CardStock>,
 ) {
-    for (entity, mut deck) in query.iter_mut() {
-        if deck.0.is_finished() {
-            let handle = std::mem::replace(&mut deck.0, runtime.0.spawn(async { None }));
-            commands.entity(entity).despawn();
-            if let Some(result) = runtime.0.block_on(handle).ok().flatten() {
-                new_pile(
-                    result.commanders,
-                    card_stock.0.clone_weak(),
-                    &mut materials,
-                    &mut commands,
-                    card_back.0.clone_weak(),
-                    -1000.0,
-                );
-                new_pile(
-                    result.main,
-                    card_stock.0.clone_weak(),
-                    &mut materials,
-                    &mut commands,
-                    card_back.0.clone_weak(),
-                    -500.0,
-                );
-                new_pile(
-                    result.side,
-                    card_stock.0.clone_weak(),
-                    &mut materials,
-                    &mut commands,
-                    card_back.0.clone_weak(),
-                    500.0,
-                );
-                new_pile(
-                    result.tokens,
-                    card_stock.0.clone_weak(),
-                    &mut materials,
-                    &mut commands,
-                    card_back.0.clone_weak(),
-                    1000.0,
-                );
-            }
+    let (entity, mut deck) = query.into_inner();
+    if deck.0.is_finished() {
+        let handle = std::mem::replace(&mut deck.0, runtime.0.spawn(async { None }));
+        commands.entity(entity).despawn();
+        if let Some(result) = runtime.0.block_on(handle).ok().flatten() {
+            new_pile(
+                result.commanders,
+                card_stock.0.clone_weak(),
+                &mut materials,
+                &mut commands,
+                card_back.0.clone_weak(),
+                -1000.0,
+                0.0,
+            );
+            new_pile(
+                result.main,
+                card_stock.0.clone_weak(),
+                &mut materials,
+                &mut commands,
+                card_back.0.clone_weak(),
+                -500.0,
+                0.0,
+            );
+            new_pile(
+                result.side,
+                card_stock.0.clone_weak(),
+                &mut materials,
+                &mut commands,
+                card_back.0.clone_weak(),
+                500.0,
+                0.0,
+            );
+            new_pile(
+                result.tokens,
+                card_stock.0.clone_weak(),
+                &mut materials,
+                &mut commands,
+                card_back.0.clone_weak(),
+                1000.0,
+                0.0,
+            );
         }
     }
+}
+fn make_material(
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    top: Handle<Image>,
+) -> Handle<StandardMaterial> {
+    materials.add(StandardMaterial {
+        base_color_texture: Some(top),
+        alpha_mode: AlphaMode::Opaque,
+        unlit: true,
+        ..default()
+    })
 }
 fn new_pile(
     pile: Vec<Card>,
@@ -343,19 +435,24 @@ fn new_pile(
     commands: &mut Commands,
     card_back: Handle<StandardMaterial>,
     x: f32,
+    z: f32,
 ) {
-    let top = pile[0].image.clone_weak();
-    let material_handle = materials.add(StandardMaterial {
-        base_color_texture: Some(top),
-        alpha_mode: AlphaMode::Opaque,
-        unlit: true,
-        ..default()
-    });
-    let mut transform = Transform::from_xyz(x, 0.0, 0.0);
+    if pile.is_empty() {
+        return;
+    }
+    let top = pile.last().unwrap().image.clone_weak();
+    let material_handle = make_material(materials, top);
+    let mut transform = Transform::from_xyz(x, 1.0, z);
     transform.rotate_x(-PI / 2.0);
     transform.rotate_y(PI);
     commands
-        .spawn((Pile(pile), transform, Visibility::default()))
+        .spawn((
+            Pile(pile),
+            transform,
+            Visibility::default(),
+            RigidBody::Dynamic,
+            Collider::cuboid(CARD_WIDTH / 2.0, CARD_HEIGHT / 2.0, 1.0),
+        ))
         .with_children(|parent| {
             parent.spawn((
                 Mesh3d(card_stock.clone_weak()),
