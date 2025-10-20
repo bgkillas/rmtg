885029cb82a7fa605240e582dee49ab1994e0a8a
bgkillas <walhelm@proton.me>
2025-10-19 23:46:29 -0400

    dont open console on windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![windows_subsystem = "windows"]
 pub fn main() {
+    #[cfg(target_os = "windows")]
+    unsafe {
+        winapi::um::wincon::AttachConsole(winapi::um::wincon::ATTACH_PARENT_PROCESS);
+    }
     rmtg::start()
 }
